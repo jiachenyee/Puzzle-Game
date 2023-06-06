9f0e04f3c6495e9e014e3efcb96025ac87560b75
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-06-06 11:06:50 +0700

    Initial Commit

---
+++ Puzzle Game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Puzzle Game
+//
+//  Created by Jia Chen Yee on 6/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Puzzle Game/Puzzle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle_GameApp.swift
+//  Puzzle Game
+//
+//  Created by Jia Chen Yee on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
